7b765296d682744de5be6b3532c503852f8578ae
AnantaNeupane <neupaneananta18@gmail.com>
2021-03-20 01:42:41 +0545

    sending data

--- shoppingapp/app/src/main/java/com/example/shoppingapp/MainActivity.java
+++ shoppingapp/app/src/main/java/com/example/shoppingapp/MainActivity.java
@@ -12,3 +12,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+    startActivityForResult(intent, ITEM_REQUEST);
+}
+
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == ITEM_REQUEST) {
+        if (resultCode == RESULT_OK) {
+        String itemString = data.getStringExtra(SecondActivity.EXTRA_ITEMS);
+        itemsList.add(itemString);
+        int i;
+        for (i = 0; i < itemsList.size(); i++) {
+        if (itemsList.size() > 10) {
+        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
+        break;
+        }
+        item[i].setVisibility(View.VISIBLE);
+        item[i].setText(itemsList.get(i));
+        }
+
+
+
+        }
+        }
+        }
+        } 
