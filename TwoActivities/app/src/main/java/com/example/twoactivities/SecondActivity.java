e9e6005cc98cc94a7027bdc9c6ba337e16576bea
AnantaNeupane <neupaneananta18@gmail.com>
2021-03-20 01:08:44 +0545

    restored activity instance

--- TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -10,24 +10,32 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG
+            = SecondActivity.class.getSimpleName();
     public static final String EXTRA_REPLY =
             "com.example.android.twoactivities.extra.REPLY";
+
     private EditText mReply;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
-        // Log the start of the onCreate() method.
-        Log.d(LOG_TAG, "-------");
-        Log.d(LOG_TAG, "onCreate");
+        mReply = findViewById(R.id.editText_second);
 
-        // Initialize all the view variables.
-        View mMessageEditText = findViewById(R.id.editText_main);
-        View mReplyHeadTextView = findViewById(R.id.text_header_reply);
-        View mReplyTextView = findViewById(R.id.text_message_reply);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
     }
 
     public void returnReply(View view) {
@@ -35,7 +43,37 @@ public class SecondActivity extends AppCompatActivity {
         Intent replyIntent = new Intent();
         replyIntent.putExtra(EXTRA_REPLY, reply);
         setResult(RESULT_OK, replyIntent);
+        Log.d(LOG_TAG, "End SecondActivity");
         finish();
     }
-    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
 }
